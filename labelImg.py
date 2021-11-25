5324602e37bdb59fd8a1d7f08a171893b93ed5b7
Flarinli <maxim.v.filatov@outlook.com>
2021-11-25 17:49:30 +0300

    zoom is bad:(

--- labelImg.py
+++ labelImg.py
@@ -316,7 +316,7 @@ class MainWindow(QMainWindow, WindowMixin):
         # Group zoom controls into a list for easier toggling.
         zoom_actions = (self.zoom_widget, zoom_in, zoom_out,
                         zoom_org, fit_window, fit_width)
-        self.zoom_mode = self.MANUAL_ZOOM
+        self.zoom_mode = self.FIT_WINDOW
         self.scalers = {
             self.FIT_WINDOW: self.scale_fit_window,
             self.FIT_WIDTH: self.scale_fit_width,
@@ -974,7 +974,7 @@ class MainWindow(QMainWindow, WindowMixin):
     def add_zoom(self, increment=10):
         self.set_zoom(self.zoom_widget.value() + increment)
 
-    def zoom_request(self, delta):
+    def zoom_request(self, delta):  # TODO: there is zoom
         # get the current scrollbar positions
         # calculate the percentages ~ coordinates
         h_bar = self.scroll_bars[Qt.Horizontal]
@@ -1025,6 +1025,83 @@ class MainWindow(QMainWindow, WindowMixin):
 
         h_bar.setValue(int(new_h_bar_value))
         v_bar.setValue(int(new_v_bar_value))
+
+
+    def zoom_request(self, delta):  # TODO: there is my zoom
+        # get the current scrollbar positions
+        # calculate the percentages ~ coordinates
+        h_bar = self.scroll_bars[Qt.Horizontal]
+        v_bar = self.scroll_bars[Qt.Vertical]
+
+        # get the current maximum, to know the difference after zooming
+        h_bar_max = h_bar.maximum()
+        v_bar_max = v_bar.maximum()
+
+        # get the cursor position and canvas size
+        # calculate the desired movement from 0 to 1
+        # where 0 = move left
+        #       1 = move right
+        # up and down analogous
+        cursor = QCursor()
+        pos = cursor.pos()
+        relative_pos = QWidget.mapFromGlobal(self, pos)
+
+        cursor_x = relative_pos.x()
+        cursor_y = relative_pos.y()
+
+        w = self.scroll_area.width()
+        h = self.scroll_area.height()
+
+        # the scaling from 0 to 1 has some padding
+        # you don't have to hit the very leftmost pixel for a maximum-left movement
+        margin = 0.1
+        # move_x = (cursor_x - margin * w) / (w - 2 * margin * w)
+        # move_y = (cursor_y - margin * h) / (h - 2 * margin * h)
+        move_x = cursor_x - (h_bar_max - h_bar.value()) / 2
+        move_y = cursor_y - (v_bar_max - v_bar.value()) / 2
+
+        # clamp the values from 0 to 1
+        move_x = min(max(move_x, 0), 1)
+        move_y = min(max(move_y, 0), 1)
+
+        # zoom in
+        units = delta / (8 * 15)
+        scale = 10
+        self.add_zoom(scale * units)
+
+        # get the difference in scrollbar values
+        # this is how far we can move
+        d_h_bar_max = h_bar.maximum() - h_bar_max
+        d_v_bar_max = v_bar.maximum() - v_bar_max
+
+        # get the new scrollbar values
+        new_h_bar_value = h_bar.value() + move_x * d_h_bar_max
+        new_v_bar_value = v_bar.value() + move_y * d_v_bar_max
+
+        h_bar.setValue(int(new_h_bar_value))
+        v_bar.setValue(int(new_v_bar_value))
+        print(f"""
+Step:
+    h_bar_max: {h_bar_max}
+    v_bar_max: {v_bar_max}
+
+    cursor_pos: {pos}
+    relative_pos: {relative_pos}
+    w: {w}
+    h: {h}
+
+    move_x: {move_x}
+    move_y: {move_y}
+    units: {units}
+    scale: {scale}
+
+    d_h_bar_max: {d_h_bar_max}
+    d_v_bar_max: {d_v_bar_max}
+
+    new_h_bar_value: {new_h_bar_value}
+    new_v_bar_value: {new_v_bar_value}
+
+    """)
 
     def set_fit_window(self, value=True):
         if value:
